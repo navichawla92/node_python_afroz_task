9083e1ff36f2612eb20acebafb80044a15242a54
navichawla92 <navichawla92@gmail.com>
2020-12-23 14:42:35 +0530

    changes for if album id is not valid

--- node/readME.md
+++ node/readME.md
@@ -1,14 +1,16 @@
 To setup the project:
 
 1) npm install
-2) set up enviroment variables: i.e GOOGLE_SERVICE_ACCOUNT_EMAIL, GOOGLE_PRIVATE_KEY, sheet
+2) set up enviroment variables: i.e GOOGLE_SERVICE_ACCOUNT_EMAIL, GOOGLE_PRIVATE_KEY, API_TOKEN, GOOGLE_SHEET_ID
 3) npm run dev
+4) To access the api pass the "x-access-token" in header and albums varibale as array 
 
 Steps to get GOOGLE_SERVICE_ACCOUNT_EMAIL and  GOOGLE_PRIVATE_KEY:
 
-![Alt text](/setup.png?raw=true "Setup for google Account")
+![Alt text](setup.png?raw=true "Setup for google Account")
 
 TO get the sheet from the url of the google sheet i.e:
+
 https://docs.google.com/spreadsheets/d/1NO5Yjzb0T8HB9HGiVrQL_DRJL7WBreW-w4nhh6vA5Ag/edit#gid=0
 
 here 1NO5Yjzb0T8HB9HGiVrQL_DRJL7WBreW-w4nhh6vA5Ag is the sheet( id of the sheet).
--- node/routers/v1/log.js
+++ node/routers/v1/log.js
@@ -1,8 +1,9 @@
-
 const logs = require('express').Router();
 const axios = require("axios");
+const urlExist = require("url-exist");
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const { authJwt } = require("../middleware");
+const API_URL = 'https://jsonplaceholder.typicode.com'
 
 logs.post("/logs", 
     [
@@ -12,6 +13,12 @@ logs.post("/logs",
     let error = null
     let doc = undefined
     const albums = req.body.albums
+    if(!Array.isArray(albums)){
+        res.json({
+            status: false,
+            data: 'Please provide albums as array.'
+        });
+    }
     try {
         doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
         await doc.useServiceAccountAuth({
@@ -27,8 +34,79 @@ logs.post("/logs",
     }
     if (albums &&  error === null) {
         let axiosAlbum = []
+        for (let index = 0; index < albums.length; index++) {
+            let url = `${API_URL}/albums/${albums[index]}`
+            const checkUrl = await urlExist(url)
+            if (checkUrl) {
+                let instance = axios.get(url)
+                axiosAlbum.push(instance)
+            }
+        }
+        if (axiosAlbum.length > 0) {
+            const results = await axios.all(axiosAlbum).then(axios.spread((...responses) => {
+                const responseObj = responses.map(item => item.data)
+                return {
+                    status: true,
+                    data: responseObj
+                }
+            })).catch(errors => {
+                return {
+                    status: false,
+                    data: errors
+                }
+            })
+
+            if (results && results.status) {
+                let axiosUsers = []
+                let info = []
+                results.data.forEach((item) => {
+                    info.push({
+                        title: item.title
+                    })
+                    let userinstance = axios.get(`${API_URL}/users/${item.userId}`)
+                    axiosUsers.push(userinstance)
+                })
+                const userResults = await axios.all(axiosUsers).then(axios.spread((...responses) => {
+                    const responseObj = responses.map(item => item.data)
+                    return {
+                        status: true,
+                        data: responseObj
+                    }
+                })).catch(errors => {
+                    return {
+                        status: false,
+                        data: errors
+                    }
+                })
+                if (userResults && userResults.status) {
+                    const finalData = info.map((item, index) => {
+                        return { ...item, username: userResults.data[index].username }
+                    })
+                    const sheet = doc.sheetsByIndex[0]
+                    const rows = await sheet.getRows();
+                    await sheet.addRows(finalData)
+                    const rowsupdated = await sheet.getRows();
+                    return res.json({
+                        status: true,
+                        data: finalData
+                    });
+                } else {
+                    return res.json({
+                        status: true,
+                        data: userResults
+                    });
+                }
+            } else {
+                return res.json({
+                    status: true,
+                    data: results
+                });
+            }
+        } else {
+            error = 'One of the album id is not valid.'
+        }
         albums.forEach(id => {
-            let instance = axios.get(`https://jsonplaceholder.typicode.com/albums/${id}`)
+            let instance = axios.get(`${API_URL}/albums/${id}`)
             axiosAlbum.push(instance)
         });
         const results = await axios.all(axiosAlbum).then(axios.spread((...responses) => {
@@ -43,54 +121,7 @@ logs.post("/logs",
                 data: errors
             }
         })
-        if (results && results.status) {
-            let axiosUsers = []
-            let info = []
-            results.data.forEach((item) => {
-                info.push({
-                    title: item.title
-                })
-                let userinstance = axios.get(` https://jsonplaceholder.typicode.com/users/${item.userId}`)
-                axiosUsers.push(userinstance)
-            })
-            const userResults = await axios.all(axiosUsers).then(axios.spread((...responses) => {
-                const responseObj = responses.map(item => item.data)
-                return {
-                    status: true,
-                    data: responseObj
-                }
-            })).catch(errors => {
-                return {
-                    status: false,
-                    data: errors
-                }
-            })
-            if (userResults && userResults.status) {
-                const finalData = info.map((item, index) => {
-                    return { ...item, username: userResults.data[index].username }
-                })
-                const sheet = doc.sheetsByIndex[0]
 
-                const headerRows = await sheet.setHeaderRow(['title', 'username']);
-                const rows = await sheet.getRows();
-                await sheet.addRows(finalData)
-                const rowsupdated = await sheet.getRows();
-                return res.json({
-                    status: true,
-                    data: finalData
-                });
-            } else {
-                return res.json({
-                    status: true,
-                    data: results
-                });
-            }
-        } else {
-            return res.json({
-                status: true,
-                data: "userResults"
-            });
-        }
     } else if (!albums) {
         error = 'Album id is required'
     }
