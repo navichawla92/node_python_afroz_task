fa43125e7d4407b5f3ce1df7b88d15d6b4181331
navichawla92 <navichawla92@gmail.com>
2020-12-25 13:11:36 +0530

    changes for auth token in node

--- node/routers/v1/log.js
+++ node/routers/v1/log.js
@@ -3,74 +3,37 @@ const axios = require("axios");
 const urlExist = require("url-exist");
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const { authJwt } = require("../middleware");
+
 // Third Party URl
 const API_URL = 'https://jsonplaceholder.typicode.com'
 
-logs.post("/logs", 
+logs.post("/logs",
     [
-      authJwt.verifyToken
+        authJwt.verifyToken
     ],
     async (req, res, next) => {
-    let error = null
-    let doc = undefined
-    const albums = req.body.albums
-    if(!Array.isArray(albums)){
-        res.json({
-            status: false,
-            data: 'Please provide albums as array.'
-        });
-    }
-    try {
-        // pass the google sheet it to GoogleSpreadsheet
-        doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
-        await doc.useServiceAccountAuth({
-            client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY,
-        });
-        doc.loadInfo();
-    } catch (errors) {
-        // if any error in google sheet api
-        error = {
-            message: errors.message,
-            name: errors.name
+        let error = null;
+        let albums = req.body.albums
+        if (!Array.isArray(albums)) {
+            res.json({
+                status: false,
+                data: 'Please provide albums as array.'
+            });
         }
-    }
-    if (albums &&  error === null) {
-        let axiosAlbum = []
-        for (let index = 0; index < albums.length; index++) {
-            let url = `${API_URL}/albums/${albums[index]}`
-            // check for url exist
-            const checkUrl = await urlExist(url);
-            if (checkUrl) {
-                let instance = axios.get(url)
-                axiosAlbum.push(instance)
+        if (albums && error === null) {
+            let axiosAlbum = []
+            albums = [...new Set(albums)];
+            for (let index = 0; index < albums.length; index++) {
+                let url = `${API_URL}/albums/${albums[index]}`
+                // check for url exist
+                const checkUrl = await urlExist(url);
+                if (checkUrl) {
+                    let instance = axios.get(url)
+                    axiosAlbum.push(instance)
+                }
             }
-        }
-        if (axiosAlbum.length > 0) {
-            const results = await axios.all(axiosAlbum).then(axios.spread((...responses) => {
-                const responseObj = responses.map(item => item.data)
-                return {
-                    status: true,
-                    data: responseObj
-                }
-            })).catch(errors => {
-                return {
-                    status: false,
-                    data: errors
-                }
-            })
-
-            if (results && results.status) {
-                let axiosUsers = []
-                let info = []
-                results.data.forEach((item) => {
-                    info.push({
-                        title: item.title
-                    })
-                    let userinstance = axios.get(`${API_URL}/users/${item.userId}`)
-                    axiosUsers.push(userinstance)
-                })
-                const userResults = await axios.all(axiosUsers).then(axios.spread((...responses) => {
+            if (axiosAlbum.length > 0) {
+                const results = await axios.all(axiosAlbum).then(axios.spread((...responses) => {
                     const responseObj = responses.map(item => item.data)
                     return {
                         status: true,
@@ -82,57 +45,84 @@ logs.post("/logs",
                         data: errors
                     }
                 })
-                if (userResults && userResults.status) {
-                    const finalData = info.map((item, index) => {
-                        return { ...item, username: userResults.data[index].username }
+
+                if (results && results.status) {
+                    let axiosUsers = []
+                    let info = []
+                    results.data.forEach((item) => {
+                        info.push({
+                            title: item.title
+                        })
+                        let userinstance = axios.get(`${API_URL}/users/${item.userId}`)
+                        axiosUsers.push(userinstance)
                     })
-                    const sheet = doc.sheetsByIndex[0]
-                    const rows = await sheet.getRows();
-                    await sheet.addRows(finalData)
-                    const rowsupdated = await sheet.getRows();
-                    return res.json({
-                        status: true,
-                        data: finalData
-                    });
+                    const userResults = await axios.all(axiosUsers).then(axios.spread((...responses) => {
+                        const responseObj = responses.map(item => item.data)
+                        return {
+                            status: true,
+                            data: responseObj
+                        }
+                    })).catch(errors => {
+                        return {
+                            status: false,
+                            data: errors
+                        }
+                    })
+                    if (userResults && userResults.status) {
+                        const finalData = info.map((item, index) => {
+                            return { ...item, username: userResults.data[index].username }
+                        })
+                        try {
+                            // pass the google sheet it to GoogleSpreadsheet
+                            // Initialization is required for every request to update the auth/access token for
+                            // request to google
+
+                            const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
+                            await doc.useServiceAccountAuth({
+                                client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+                                private_key: process.env.GOOGLE_PRIVATE_KEY,
+                            });
+                            await doc.loadInfo();
+                            const sheet = doc.sheetsByIndex[0]
+                            await sheet.addRows(finalData)
+                            return res.json({
+                                status: true,
+                                data: finalData
+                            });
+                        } catch (errors) {
+                            // if any error in google sheet api
+                            error = {
+                                message: errors.message,
+                                name: errors.name,
+                                stack: errors.stack
+                            }
+                            return res.json({
+                                status: false,
+                                data: error
+                            });
+                        }
+                    } else {
+                        return res.json({
+                            status: true,
+                            data: userResults
+                        });
+                    }
                 } else {
                     return res.json({
                         status: true,
-                        data: userResults
+                        data: results
                     });
                 }
             } else {
-                return res.json({
-                    status: true,
-                    data: results
-                });
+                error = 'One of the album id is not valid.'
             }
-        } else {
-            error = 'One of the album id is not valid.'
+        } else if (!albums) {
+            error = 'Album id is required'
         }
-        albums.forEach(id => {
-            let instance = axios.get(`${API_URL}/albums/${id}`)
-            axiosAlbum.push(instance)
+        res.json({
+            status: false,
+            data: error
         });
-        const results = await axios.all(axiosAlbum).then(axios.spread((...responses) => {
-            const responseObj = responses.map(item => item.data)
-            return {
-                status: true,
-                data: responseObj
-            }
-        })).catch(errors => {
-            return {
-                status: false,
-                data: errors
-            }
-        })
-
-    } else if (!albums) {
-        error = 'Album id is required'
-    }
-    res.json({
-        status: false,
-        data: error
     });
-});
 
 module.exports = logs
